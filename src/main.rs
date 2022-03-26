6d342187cced14e23a40303b0ee4c8a376760659
Phil Kulak <phil.kulak@vevo.com>
2022-03-26 08:22:06 -0700

    Get setup for multiple bots.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use std::env;
+
 mod webhook;
 mod bots;
 mod matrix;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    bots::home::main().await?;
+    if let Some(bot) = env::args().nth(1) {
+        match bot.as_str() {
+            "home" => bots::home::main().await?,
+            _ => {
+                println!("unknown bot: {}", bot);
+                return Ok(());
+            }
+        }
+    }
+
+    println!("usage: bots {{bot name}}");
 
     Ok(())
 }
